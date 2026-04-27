3cd799698a7c8b59dfa8f8db7adb35ea8989854e
mh680298-byte <mh680298@gmail.com>
2026-04-27 17:46:50 +0330

    Update MainActivity.java

--- app/src/main/java/com/example/exampleapp/MainActivity.java
+++ app/src/main/java/com/example/exampleapp/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.exampleapp.databinding.ActivityMainBinding;
+import com.example.myfirstapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     /*
